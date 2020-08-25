c032b63c997b3bab20cde957731bdb14c92b8a1e
Carlos Nardo <c.cesar.nardo@gmail.com>
2020-08-24 21:18:06 -0300

    Estilo do retangulo do item. Array para repetir

--- pages/lista-de-presentes.jsx
+++ pages/lista-de-presentes.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const items = new Array(10);
+
 const ListaDePresentesPage = () => {
     return (
         <>
@@ -7,25 +9,29 @@ const ListaDePresentesPage = () => {
                 <img src="header2.png" alt="Header do site" />
             </figure>
             <main className="container">
-                <div className="retanguloDoItem">
-                    <div className="caixaDaFoto">
-                        <figure>
-                            <img
-                                src="fotosDosPresentes/banheirinha.jpg"
-                            />
-                        </figure>
-                    </div>
-                    <div className="caixaDoConteudo">
-                        <div className="itemDePresente">
-                            <p class="nomeDoItem">Banheirinha</p>
-                            <p class="valorDoItem">Valor Aproximado: R$ 250,00</p>
-                           
+                {
+                    items.fill(1).map(() => (
+                        <div className="retanguloDoItem">
+                            <div className="caixaDaFoto">
+                                <figure>
+                                    <img
+                                        src="fotosDosPresentes/banheirinha.jpg"
+                                    />
+                                </figure>
+                            </div>
+                            <div className="caixaDoConteudo">
+                                <div className="itemDePresente">
+                                    <p className="nomeDoItem">Banheirinha</p>
+                                    <p className="valorDoItem">Valor Aproximado: R$ 250,00</p>
+
+                                </div>
+                                <div className="areaBotao">
+                                    <button className="botaoDarEsse">Deixa esse Comigo!</button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="areaBotao">
-                            <button className="botaoDarEsse">Deixa esse Comigo!</button>
-                        </div>  
-                    </div>
-                </div>
+                    ))
+                }
             </main>
         </>
     );
