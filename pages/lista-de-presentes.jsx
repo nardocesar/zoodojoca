3b879b5780c10f62f99dbe9a6351dc826924bb1d
Carlos Nardo <c.cesar.nardo@gmail.com>
2020-08-25 15:53:25 -0300

    Fix drop item

--- pages/lista-de-presentes.jsx
+++ pages/lista-de-presentes.jsx
@@ -3,93 +3,80 @@ import React, { useState } from "react";
 const items = new Array(10);
 
 const ListaDePresentesPage = () => {
-    return (
-        <>
-            <figure className="headerLista">
-                <img src="header2.png" alt="Header do site" />
-            </figure>
-            <main className="container">
-                {items.fill(1).map((_, index) => {
-                    const [colapsed, setColapsed] = useState(true);
+	return (
+		<>
+			<figure className="headerLista">
+				<img src="header2.png" alt="Header do site" />
+			</figure>
+			<main className="container">
+				{items.fill(1).map((_, index) => {
+					const [colapsed, setColapsed] = useState(true);
 
-                    return (
-                        <div
-                            key={index}
-                            className={
-                                colapsed
-                                    ? "retanguloDoItem"
-                                    : "retanguloDoItem expanded"
-                            }
-                        >
-                            {colapsed ? (
-                                // FECHADO
-                                <>
-                                    <div className="caixaDaFoto">
-                                        <figure>
-                                            <img src="fotosDosPresentes/banheirinha.jpg" />
-                                        </figure>
-                                    </div>
-                                    <div className="caixaDoConteudo">
-                                        <div className="itemDePresente">
-                                            <p className="nomeDoItem">
-                                                Banheirinha
-                                            </p>
-                                            <p className="valorDoItem">
-                                                Valor Aproximado: R$ 250,00
-                                            </p>
-                                        </div>
-                                        <div className="areaBotao">
-                                            <button
-                                                className="botaoDarEsse"
-                                                onClick={() =>
-                                                    colapsed
-                                                        ? setColapsed(false)
-                                                        : setColapsed(true)
-                                                }
-                                            >
-                                                Eu vou dar esse!
-                                            </button>
-                                        </div>
-                                    </div>
-                                </>
-                            ) : (
-                                // ABERTO
-                                <>
-                                    <div className="caixaDaFoto">
-                                        <figure>
-                                            <img src="fotosDosPresentes/banheirinha.jpg" />
-                                        </figure>
-                                    </div>
-                                    <div className="caixaDoConteudo">
-                                        <div className="itemDePresente">
-                                            <p className="nomeDoItem">
-                                                Banheirinha
-                                            </p>
-                                            <p className="valorDoItem">
-                                                Valor Aproximado: R$ 250,00
-                                            </p>
-                                        </div>
-                                        <div className="areaBotao">
-                                            <button
-                                                className="botaoDarEsse"
-                                                onClick={() =>
-                                                    colapsed
-                                                        ? setColapsed(false)
-                                                        : setColapsed(true)
-                                                }
-                                            >
-                                                Deixa esse Comigo!
-                                            </button>
-                                        </div>
-                                    </div>
-                                </>
-                            )}
-                        </div>
-                    );
-                })}
-            </main>
-        </>
-    );
+					return (
+						<div
+							key={index}
+							className={
+								colapsed ? "retanguloDoItem" : "retanguloDoItem expanded"
+							}
+						>
+							{colapsed ? (
+								// FECHADO
+								<>
+									<div className="caixaDaFoto">
+										<figure>
+											<img src="fotosDosPresentes/banheirinha.jpg" />
+										</figure>
+									</div>
+									<div className="caixaDoConteudo">
+										<div className="itemDePresente">
+											<p className="nomeDoItem">Banheirinha</p>
+											<p className="valorDoItem">Valor Aproximado: R$ 250,00</p>
+										</div>
+										<div className="areaBotao">
+											<button
+												className="botaoDarEsse"
+												onClick={() =>
+													colapsed ? setColapsed(false) : setColapsed(true)
+												}
+											>
+												Eu vou dar esse!
+											</button>
+										</div>
+									</div>
+								</>
+							) : (
+								// ABERTO
+								<>
+									<div className="headerDoItem">
+										<div className="caixaDaFoto">
+											<figure>
+												<img src="fotosDosPresentes/banheirinha.jpg" />
+											</figure>
+										</div>
+										<div className="tituloDoItem">
+											<p className="nomeDoItem">Banheirinha</p>
+											<p className="valorDoItem">Valor Aproximado: R$ 250,00</p>
+										</div>
+									</div>
+									<div className="descricaoDoItem">
+										<p>Lorem ipsum dolor sit amet</p>
+										<button
+											className="botaoDarEsse"
+											onClick={() =>
+												colapsed ? setColapsed(false) : setColapsed(true)
+											}
+										>
+											Deixa esse comigo!
+										</button>
+									</div>
+								</>
+							)}
+						</div>
+					);
+				})}
+			</main>
+		</>
+	);
 };
 
 export default ListaDePresentesPage;
