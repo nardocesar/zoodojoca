ed54bc468bd15f57dcb9e8b097a1364a623f1e9a
Carlos Nardo <c.cesar.nardo@gmail.com>
2020-09-05 18:13:58 -0300

    deleting res.set

--- pages/api/presentes.js
+++ pages/api/presentes.js
@@ -3,7 +3,6 @@ import {
 } from '../../firebase';
 
 export default async (req, res) => {
-	res.set('Cache-Control', 'no-cache');
 	const list = await read('presentes', 'lista');
 	res.json(list);
 }
