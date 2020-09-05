dd2691d4498ffecf1c2d2fd068ecd43d06d942db
Carlos Nardo <c.cesar.nardo@gmail.com>
2020-09-05 18:08:35 -0300

    Force no-cache

--- pages/api/presentes.js
+++ pages/api/presentes.js
@@ -3,6 +3,7 @@ import {
 } from '../../firebase';
 
 export default async (req, res) => {
+	res.set('Cache-Control', 'no-cache');
 	const list = await read('presentes', 'lista');
 	res.json(list);
 }
